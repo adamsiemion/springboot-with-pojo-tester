048c13c3e176a82380ffc70aa2823c60963d4e91
Adam Siemion <adam.siemion+github@gmail.com>
2017-03-03 23:18:32 +0100

    skeleton from start.spring.io

---
+++ src/main/java/com/siemion/SpringbootWithPojoTesterApplication.java
@@ -0,0 +1,12 @@
+package com.siemion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithPojoTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithPojoTesterApplication.class, args);
+	}
+}
